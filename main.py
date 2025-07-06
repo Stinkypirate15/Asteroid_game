77afacb779127b12ee98b2ada9503daa88ada96c
Stinkypirate15 <13579268cd@gmail.com>
2025-07-06 08:00:49 -0400

    game now uses less device resources

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ from constants import *
 
 def main():
     pygame.init()
+    clock=pygame.time.Clock()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt=clock.tick(60)/1000
         screen.fill("black")
         pygame.display.flip()
+        
+        
+    
+   
+
+    
+
+    
+
 
 
 if __name__ == "__main__":
