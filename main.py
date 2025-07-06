cc4826d54430acbf76ee55d5313b1d1e34f4ddc0
Stinkypirate15 <13579268cd@gmail.com>
2025-07-06 11:07:26 -0400

    added player to code

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock=pygame.time.Clock()
+    new_player=player(
+    x = SCREEN_WIDTH / 2,
+    y = SCREEN_HEIGHT / 2
+)
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt=clock.tick(60)/1000
         screen.fill("black")
+        new_player.draw(screen)
         pygame.display.flip()
         
         
